ef2c3ab486b8e4992373f03e2fa96616880c57cf
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-08-13 12:50:50 +0200

    feat: add typescript definitions

---
+++ types.d.ts
@@ -0,0 +1,26 @@
+export declare type Operator = {
+    type: string
+    countryName: string
+    countryCode: string
+    mcc: string
+    mnc: string
+    brand: string
+    operator: string
+    status: string
+    bands: string
+    notes: string
+}
+
+export declare const all: () => Operator[];
+
+export declare const filter: (filters?: {
+    countryCode?: string
+    mcc?: string
+    mnc?: string
+    mccmnc?: string
+    statusCode?: string
+}) => Operator[];
+
+export declare const statusCodes: () => string[];
+
+export {};
